8ed8e74ba9742cfb6e324684ddfafe71a5139b00
Rebel <gahlotpercy@gmail.com>
2013-08-11 17:19:21 +0530

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -58,3 +58,6 @@ endif
 ifeq (pa_find5,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_find5.mk
 endif
+ifeq (pa_nypon,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_nypon.mk
+endif
